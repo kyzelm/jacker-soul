d06ba70a5f58e3152b8bdd6926c3256830824ec4
Damian Osiński <osinskidamian03@gmail.com>
2025-04-15 18:45:14 +0200

    Chopaki to działa, havok żyje

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import './App.css'
 import {useGamepadController} from "./hooks/gamepadControllerHook.ts";
-import Menu from "./components/Menu/Menu.tsx";
+// import Menu from "./components/Menu/Menu.tsx";
 import GameEngine from "./components/GameEngine/GameEngine.tsx";
-import ControllerModal from "./components/Modal/ControllerModal.tsx";
-import {useAppSelector} from "./store/store.ts";
-import {MenuPages} from "./store/menuSlice.ts";
+// import ControllerModal from "./components/Modal/ControllerModal.tsx";
+// import {useAppSelector} from "./store/store.ts";
+// import {MenuPages} from "./store/menuSlice.ts";
 
 function App() {
-  const isControllerConnected = useAppSelector(state => state.gamepad.isConnected)
-  const currentPage = useAppSelector(state => state.menu.currentPage)
+  // const isControllerConnected = useAppSelector(state => state.gamepad.isConnected)
+  // const currentPage = useAppSelector(state => state.menu.currentPage)
 
   useGamepadController()
 
   return (
     <>
-      {!isControllerConnected && <ControllerModal/>}
-      {currentPage !== MenuPages.NONE && <Menu/>}
+      {/*{!isControllerConnected && <ControllerModal/>}*/}
+      {/*{currentPage !== MenuPages.NONE && <Menu/>}*/}
       <GameEngine/>
     </>
   )
--- src/components/GameEngine/GameEngine.tsx
+++ src/components/GameEngine/GameEngine.tsx
@@ -1,41 +1,96 @@
-import {JSX, useEffect} from "react";
-import SceneComponent from "babylonjs-hook";
-import {FreeCamera, HemisphericLight, ImportMeshAsync, Scene, Vector3} from "@babylonjs/core";
-import styles from "./GameEngine.module.css";
-import {useAppSelector} from "../../store/store.ts";
+import {JSX, useEffect, useRef} from "react";
+import {
+  Engine,
+  FreeCamera,
+  HavokPlugin,
+  HemisphericLight, ImportMeshAsync, ISceneLoaderAsyncResult, Mesh,
+  MeshBuilder,
+  PhysicsAggregate,
+  PhysicsShapeType,
+  Scene,
+  Vector3
+} from "@babylonjs/core";
 import "@babylonjs/loaders";
+import HavokPhysics from "@babylonjs/havok";
+import styles from "./GameEngine.module.css"
 
 export default function GameEngine(): JSX.Element {
-  const buttonDown = useAppSelector(state => state.gamepad.buttonsDown)
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-  }, [buttonDown]);
+  const cameraRef = useRef<FreeCamera>(null);
+  const boxRef = useRef<Mesh>(null);
+  const mapRef = useRef<ISceneLoaderAsyncResult>(null);
 
-  const onSceneReady = async (scene: Scene) => {
-    const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+  async function SceneSetup(engine: Engine): Promise<Scene> {
+    const scene = new Scene(engine);
+
+    const camera = new FreeCamera("cam", new Vector3(0, 5, -10), scene);
     camera.setTarget(Vector3.Zero());
+    camera.attachControl(canvasRef.current, true);
+    cameraRef.current = camera;
 
-    camera.attachControl(scene.getEngine().getRenderingCanvas(), true);
-
-    const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
-    const level = await ImportMeshAsync("./models/Prototype_Level.glb", scene)
+    const map = await ImportMeshAsync("/models/level.glb", scene);
+    map.meshes.forEach((mesh) => {
+      if (mesh.parent) {
+        mesh.computeWorldMatrix();
+        mesh.parent = null;
+      }
+      mesh.isPickable = false;
+      mesh.checkCollisions = true;
+    });
 
-    console.log(level);
-  };
+    const box = MeshBuilder.CreateBox("box", {size: 1}, scene);
+    box.position = new Vector3(0, 5, 0);
 
-  const onRender = () => {
-  };
+    const hp = await HavokPhysics({
+      locateFile() {
+        return "/node_modules/@babylonjs/havok/lib/esm/HavokPhysics.wasm";
+      }
+    });
+
+    scene.enablePhysics(new Vector3(0, -9.81, 0), new HavokPlugin(true, hp));
+
+    new PhysicsAggregate(box, PhysicsShapeType.BOX, {
+      mass: 1,
+      restitution: 0,
+    }, scene);
+
+    map.meshes.forEach((mesh) => {
+      new PhysicsAggregate(mesh, PhysicsShapeType.BOX, {
+        mass: 0,
+        restitution: 0,
+      }, scene);
+    })
+
+    boxRef.current = box;
+    mapRef.current = map;
+
+    return scene;
+  }
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const engine = new Engine(canvasRef.current, true, {
+      preserveDrawingBuffer: true,
+      stencil: true,
+    })
+
+    SceneSetup(engine).then((scene) => {
+      engine.runRenderLoop(() => {
+        if (scene) {
+          scene.render();
+        }
+      })
+    }).catch((err) => {
+      console.error("Error setting up scene:", err);
+    })
+  }, []);
 
   return (
-    <SceneComponent
-      antialias={true}
-      engineOptions={{preserveDrawingBuffer: true, stencil: true}}
-      adaptToDeviceRatio={true}
-      onSceneReady={onSceneReady}
-      onRender={onRender}
-      className={styles.engine}
-    />
+    <canvas ref={canvasRef} className={styles.engine}></canvas>
   )
 }
